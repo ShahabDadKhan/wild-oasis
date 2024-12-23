2bb1c55add27e8afd0cd28a0c33fde7c3903b89c
shahabdadkhan <shahabkhan@fynd-external.com>
2024-12-23 23:55:00 +0530

    Added heading types as h1 and h2 and h3

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,24 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello World</H1>
+        <Heading as="h1">Hello World</Heading>
+        <Heading as="h2">Check In and Out</Heading>
         <Button onClick={() => alert("In")}>Check In</Button>
         <Button onClick={() => alert("Out")}>Check Out</Button>
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guests"></Input>
         <Input type="number" placeholder="Number of guests"></Input>
       </StyledApp>
     </>
   );
 }
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
 
 const StyledApp = styled.div`
   background-color: orangered;
