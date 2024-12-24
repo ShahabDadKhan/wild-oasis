80f2acdcef765f2bc288d4544b10035aba379f0f
shahabdadkhan <shahabkhan@fynd-external.com>
2024-12-24 22:33:59 +0530

    Added sidebar and header

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyle from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 export default function App() {
   return (
@@ -15,13 +16,15 @@ export default function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="Bookings" element={<Bookings />} />
-          <Route path="Cabins" element={<Cabins />} />
-          <Route path="Users" element={<Users />} />
-          <Route path="Settings" element={<Settings />} />
-          <Route path="Account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="Bookings" element={<Bookings />} />
+            <Route path="Cabins" element={<Cabins />} />
+            <Route path="Users" element={<Users />} />
+            <Route path="Settings" element={<Settings />} />
+            <Route path="Account" element={<Account />} />
+          </Route>
           <Route path="Login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
