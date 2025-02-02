19042fe2bd6c44710cc6e7f3d5b1cf74e610365c
shahabdadkhan <shahabkhan@fynd-external.com>
2025-02-03 01:28:38 +0530

    form input

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "no-unused-vars": "off", // Disable unused variables error
+    "no-unused-vars": "off", // Disable unused variables error,
+    "react/prop-types": "off",
   },
 };
